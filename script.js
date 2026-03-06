d1a273ae21fe186eb737332990ec88360475f260
Phan Tien Dat <tiendatsgu298@gmail.com>
2026-03-07 03:38:39 +0700

    OwenFitness Uprade

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let chart;
+let calorieChart;
+let macroChart;
 
 function calculate(){
 
 let weight=parseFloat(document.getElementById("weight").value);
+
 let height=parseFloat(document.getElementById("height").value);
+
 let age=parseFloat(document.getElementById("age").value);
+
 let gender=document.getElementById("gender").value;
+
 let activity=parseFloat(document.getElementById("activity").value);
+
 
 if(!weight||!height||!age){
 
 alert("Please fill all fields");
+
 return;
 
 }
+
 
 let bmr;
 
@@ -27,36 +35,73 @@ bmr=10*weight+6.25*height-5*age-161;
 
 }
 
+
 let tdee=Math.round(bmr*activity);
 
 let cut=tdee-500;
+
 let bulk=tdee+300;
 
 let bmi=(weight/((height/100)*(height/100))).toFixed(1);
 
-document.getElementById("bmr").innerText="BMR: "+Math.round(bmr)+" kcal";
-document.getElementById("tdee").innerText="TDEE: "+tdee+" kcal";
-document.getElementById("cut").innerText="Cut Calories: "+cut+" kcal";
-document.getElementById("maintain").innerText="Maintain: "+tdee+" kcal";
-document.getElementById("bulk").innerText="Bulk: "+bulk+" kcal";
-document.getElementById("bmi").innerText="BMI: "+bmi;
 
-drawChart(cut,tdee,bulk);
+/* macros */
+
+let protein=Math.round(weight*2);
+
+let fat=Math.round(weight*0.8);
+
+let proteinCal=protein*4;
+
+let fatCal=fat*9;
+
+let carbCal=tdee-proteinCal-fatCal;
+
+let carb=Math.round(carbCal/4);
+
+
+document.getElementById("bmr").innerText=Math.round(bmr);
+
+document.getElementById("tdee").innerText=tdee;
+
+document.getElementById("bmi").innerText=bmi;
+
+
+document.getElementById("cut").innerText="Cut Calories: "+cut;
+
+document.getElementById("maintain").innerText="Maintain: "+tdee;
+
+document.getElementById("bulk").innerText="Bulk: "+bulk;
+
+
+document.getElementById("protein").innerText="Protein: "+protein+" g";
+
+document.getElementById("carb").innerText="Carbs: "+carb+" g";
+
+document.getElementById("fat").innerText="Fat: "+fat+" g";
+
+
+drawCalorieChart(cut,tdee,bulk);
+
+drawMacroChart(protein,carb,fat);
+
+
+saveHistory(tdee,bmi);
 
 }
 
 
-function drawChart(cut,maintain,bulk){
+function drawCalorieChart(cut,maintain,bulk){
 
 let ctx=document.getElementById("calorieChart");
 
-if(chart){
+if(calorieChart){
 
-chart.destroy();
+calorieChart.destroy();
 
 }
 
-chart=new Chart(ctx,{
+calorieChart=new Chart(ctx,{
 
 type:"bar",
 
@@ -77,6 +122,83 @@ data:[cut,maintain,bulk]
 });
 
 }
+
+
+function drawMacroChart(protein,carb,fat){
+
+let ctx=document.getElementById("macroChart");
+
+if(macroChart){
+
+macroChart.destroy();
+
+}
+
+macroChart=new Chart(ctx,{
+
+type:"pie",
+
+data:{
+
+labels:["Protein","Carbs","Fat"],
+
+datasets:[{
+
+data:[protein,carb,fat]
+
+}]
+
+}
+
+});
+
+}
+
+
+function saveHistory(tdee,bmi){
+
+let history=JSON.parse(localStorage.getItem("history"))||[];
+
+history.push({
+
+tdee:tdee,
+
+bmi:bmi
+
+});
+
+
+localStorage.setItem("history",JSON.stringify(history));
+
+
+renderHistory();
+
+}
+
+
+function renderHistory(){
+
+let history=JSON.parse(localStorage.getItem("history"))||[];
+
+let list=document.getElementById("history");
+
+list.innerHTML="";
+
+
+history.slice(-5).reverse().forEach(item=>{
+
+let li=document.createElement("li");
+
+li.textContent="TDEE: "+item.tdee+" | BMI: "+item.bmi;
+
+list.appendChild(li);
+
+});
+
+}
+
+
+renderHistory();
 
 
 document.getElementById("darkToggle").onclick=function(){
