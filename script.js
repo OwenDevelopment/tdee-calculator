bf5b0886411f3546950c2fdf7a20ce759011f51d
Phan Tien Dat <tiendatsgu298@gmail.com>
2026-03-07 03:31:23 +0700

    OwenFitness UI upgrade

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
 function calculate(){
 
-let weight = parseFloat(document.getElementById("weight").value);
-let height = parseFloat(document.getElementById("height").value);
-let age = parseFloat(document.getElementById("age").value);
-let gender = document.getElementById("gender").value;
-let activity = parseFloat(document.getElementById("activity").value);
+let weight=parseFloat(document.getElementById("weight").value);
+let height=parseFloat(document.getElementById("height").value);
+let age=parseFloat(document.getElementById("age").value);
 
-if(!weight || !height || !age){
+let gender=document.getElementById("gender").value;
+
+let activity=parseFloat(document.getElementById("activity").value);
+
+
+if(!weight||!height||!age){
+
 alert("Please fill all fields");
+
 return;
+
 }
+
 
 let bmr;
 
-if(gender === "male"){
-bmr = 10*weight + 6.25*height - 5*age + 5;
-}else{
-bmr = 10*weight + 6.25*height - 5*age - 161;
-}
+if(gender==="male"){
 
-let tdee = Math.round(bmr * activity);
-
-let cut = tdee - 500;
-let bulk = tdee + 300;
-
-let bmi = (weight / ((height/100)*(height/100))).toFixed(1);
-
-document.getElementById("bmr").innerText = "BMR: " + Math.round(bmr) + " kcal";
-document.getElementById("tdee").innerText = "TDEE: " + tdee + " kcal";
-document.getElementById("cut").innerText = "Cutting Calories: " + cut + " kcal";
-document.getElementById("maintain").innerText = "Maintenance: " + tdee + " kcal";
-document.getElementById("bulk").innerText = "Bulking Calories: " + bulk + " kcal";
-document.getElementById("bmi").innerText = "BMI: " + bmi;
+bmr=10*weight+6.25*height-5*age+5;
 
 }
 
-document.getElementById("darkToggle").onclick = function(){
+else{
+
+bmr=10*weight+6.25*height-5*age-161;
+
+}
+
+
+let tdee=Math.round(bmr*activity);
+
+let cut=tdee-500;
+
+let bulk=tdee+300;
+
+let bmi=(weight/((height/100)*(height/100))).toFixed(1);
+
+
+document.getElementById("bmr").innerText="BMR: "+Math.round(bmr)+" kcal";
+
+document.getElementById("tdee").innerText="TDEE: "+tdee+" kcal";
+
+document.getElementById("cut").innerText="Cut Calories: "+cut+" kcal";
+
+document.getElementById("maintain").innerText="Maintain Calories: "+tdee+" kcal";
+
+document.getElementById("bulk").innerText="Bulk Calories: "+bulk+" kcal";
+
+document.getElementById("bmi").innerText="BMI: "+bmi;
+
+}
+
+
+document.getElementById("darkToggle").onclick=function(){
+
 document.body.classList.toggle("dark");
-};
+
+}
